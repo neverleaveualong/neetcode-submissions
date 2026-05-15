a1668177b8c18af751e7c1f0d6b37e373aef2477
SIMWOOHYUN <75990406+neverleaveualong@users.noreply.github.com>
2026-05-15 13:54:04 +0900

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for(int i = 0; i<s.length(); i++){
+            if(s[i] == '('){
+                stk.push(s[i]);
+            } else if(s[i] == '{'){
+                stk.push(s[i]);
+            } else if (s[i] == '['){
+                stk.push(s[i]);
+            } else if (s[i] ==')'){
+                if(!stk.empty() && stk.top() == '('){
+                    stk.pop();
+                } else{
+                    return false;
+                }
+            } else if (s[i] ==']'){
+                if(!stk.empty() && stk.top() == '['){
+                    stk.pop();
+                } else{
+                    return false;
+                }
+            } else if (s[i] =='}'){
+                if(!stk.empty() && stk.top() == '{'){
+                    stk.pop();
+                } else{
+                    return false;
+                }
+            }
+        }
+        return stk.empty();
+    }
+};
