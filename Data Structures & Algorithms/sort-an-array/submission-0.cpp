8a890dc9cafb5de11e9cd8a074868df39bbb3f1b
SIMWOOHYUN <75990406+neverleaveualong@users.noreply.github.com>
2026-05-28 16:58:53 +0900

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    void merge(vector<int> &nums, int left, int mid, int right){
+        vector<int> temp;
+
+        int i = left;
+        int j = mid + 1;
+
+        while(i <= mid && j <= right){
+            if(nums[i] < nums[j]){
+                temp.push_back(nums[i]);
+                i++;
+            } else {
+                temp.push_back(nums[j]);
+                j++;
+            }
+        }
+
+        while(i <= mid){
+            temp.push_back(nums[i]);
+            i++;
+        }
+        while(j <= right){
+            temp.push_back(nums[j]);
+            j++;
+        }
+        for(int k = 0; k<temp.size(); k++){
+            nums[left + k] = temp[k];
+        }
+    }
+
+    void sort(vector<int>& nums, int left, int right){
+        if (left >= right) return;
+
+        int mid = (left+right)/2;
+
+        sort(nums, left,mid);
+        sort(nums,mid+1,right);
+
+        merge(nums, left, mid, right);
+    }
+
+    vector<int> sortArray(vector<int>& nums) {
+        sort(nums, 0, nums.size()-1);
+        return nums;
+    }
+};
